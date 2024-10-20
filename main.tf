31b451fb1caadf4502677e6bde851fe957882a9a
khaled-projects <167363566+khaled-projects@users.noreply.github.com>
2024-10-20 13:24:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  region = "us-west-2"  # Modify to your preferred region
 }
 
 terraform {
@@ -10,17 +11,12 @@ terraform {
   }
 }
 
-
-
-
-
-
-# Create an EC2 instance without key pair and security group rules
+# Create an EC2 instance without a key pair and security group rules
 resource "aws_instance" "example_instance" {
-  ami           = "ami-0ddc798b3f1a5117e"  # Replace with a valid AMI ID
-  instance_type = "t2.micro"
-  subnet_id     = subnet-05f6b3cd6a8e39128
-  security_groups = sg-030e0f2c0e7336c77
+  ami                    = "ami-0ddc798b3f1a5117e"  # Replace with a valid AMI ID
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-05f6b3cd6a8e39128"  # Use quotes around the subnet ID
+  security_groups        = ["sg-030e0f2c0e7336c77"]     # Use quotes around the security group ID
 
   associate_public_ip_address = true  # Instance will have a public IP
 
