150bc7516829dfcf8a820104caac00a3411087bd
khaled-projects <167363566+khaled-projects@users.noreply.github.com>
2024-10-20 13:30:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,22 @@ terraform {
   }
 }
 
-# Create an EC2 instance without a key pair and security group rules
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]  # Amazon's official AMIs
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "example_instance" {
-  ami                    = "ami-06b21ccaeff8cd686"  # Replace with a valid AMI ID
-  instance_type          = "t2.micro"
-  subnet_id              = "subnet-05f6b3cd6a8e39128"  # Use quotes around the subnet ID
-  security_groups        = ["sg-030e0f2c0e7336c77"]     # Use quotes around the security group ID
+  ami                    = data.aws_ami.latest_amazon_linux.id  # Use the AMI from the data source
+  instance_type         = "t2.micro"
+  subnet_id             = "subnet-05f6b3cd6a8e39128"  # Use quotes around the subnet ID
+  security_groups       = ["sg-030e0f2c0e7336c77"]     # Use quotes around the security group ID
 
   associate_public_ip_address = true  # Instance will have a public IP
 
