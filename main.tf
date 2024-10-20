45775ba389addac0c965c1e7437dad6ff91a9dd3
khaled-projects <167363566+khaled-projects@users.noreply.github.com>
2024-10-20 12:10:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  # region     = "us-west-2"
+ # access_key = "my-access-key"
+  # secret_key = "my-secret-key"
 }
 terraform {
   required_providers {
